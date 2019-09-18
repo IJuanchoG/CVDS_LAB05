b60e8820b4f03dc2a5c710afa2605cc3845e94a0
Juancho Garcia <49318314+IJuankhoxD@users.noreply.github.com>
2019-09-17 23:52:59 -0500

    Terminada parte 3, comenzando parte 4

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CVDS_LAB05
+
+ > Integrantes: 
+ > 			Juan Carlos Garcia Garzon
+ > 			Johan Damian Garrido Florez
+## Parte I. - Jugando a ser un cliente HTTP
+
--- Servlet/src/main/java/edu/eci/cvds/servlet/OtherSampleServlet.java
+++ Servlet/src/main/java/edu/eci/cvds/servlet/OtherSampleServlet.java
@@ -4,6 +4,7 @@ import edu.eci.cvds.servlet.model.Todo;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,26 +17,46 @@ import javax.servlet.http.HttpServletResponse;
 )
 public class OtherSampleServlet extends HttpServlet{
     static final long serialVersionUID = 35L;
-
+    
     @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       Writer responseWriter = resp.getWriter();
-       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
-       String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
-       try{
-           int id = Integer.parseInt(name);
-           Todo todo = Service.getTodo(id);
-           if(todo!= null) resp.setStatus(HttpServletResponse.SC_NOT_FOUND);    
-           
-       }catch(NumberFormatException ex){
-           resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-       }catch (MalformedURLException ex){
-           resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-       }
-       
-       
-       resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-       responseWriter.write("Hello" + name + "!");
-       responseWriter.flush();
-   }
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
+        Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+        String name = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+        try{
+            int id = Integer.parseInt(name);
+            Todo todo = Service.getTodo(id);
+            if(todo!= null) resp.setStatus(HttpServletResponse.SC_NOT_FOUND); 
+            ArrayList<Todo> todoList = new ArrayList<>(); todoList.add(todo);
+            responseWriter.write(Service.todosToHTMLTable(todoList));
+            responseWriter.flush();           
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }catch (MalformedURLException ex){
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch(NumberFormatException | IOException ex){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }       
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
+        Optional<String> optId = Optional.ofNullable(req.getParameter("ID"));
+        String id_S = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "";
+        try{
+            int id = Integer.parseInt(id_S);
+            Todo todo = Service.getTodo(id);
+            if(todo!= null) resp.setStatus(HttpServletResponse.SC_NOT_FOUND); 
+            ArrayList<Todo> todoList = new ArrayList<>(); todoList.add(todo);
+            responseWriter.write(Service.todosToHTMLTable(todoList));
+            responseWriter.flush();           
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }catch (MalformedURLException ex){
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        catch(NumberFormatException | IOException ex){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }       
+    }
 }
